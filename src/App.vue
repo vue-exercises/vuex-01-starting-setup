6b561fd77ba96d55f61738192a5561e2fa59cf59
dedoblege <dedoblege@gmail.com>
2020-10-11 12:52:13 +0200

    introducing getters

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <base-container title="Vuex">
     <the-counter></the-counter>
+    <favorite-value></favorite-value>
     <change-counter></change-counter>
   </base-container>
 </template>
@@ -8,12 +9,14 @@
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
+    FavoriteValue,
     ChangeCounter
   }
 };
